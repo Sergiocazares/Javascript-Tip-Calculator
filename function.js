a4faa13327c127b3d89c23888edb214014a48f25
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-12 17:52:44 -0600

    created function get number of people incase its empty or 1

--- function.js
+++ function.js
@@ -6,4 +6,17 @@ function calulateTip() {
     var serviceQual = document.getElementById("serviceQual").value;
     var numOfPeople = document.getElementById("peopleamt").value;
 
+//Validate input
+    if (billAmt === "" || serviceQual == 0) {
+        alert("Please enter values");
+        return;
+    }
+
+//Check see if this input is empty or less than or equal to 1
+    if (numOfPeople === "" || numOfPeople <= 1) {
+        numOfPeople = 1;
+        document.getElementById("each").style.display = "none";
+    } else {
+        document.getElementById("each").style.display = "block";
+    }
 }
