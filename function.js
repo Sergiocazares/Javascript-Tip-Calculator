5ac8b8e80c43a586691747282f7cbb8169781afd
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-12 17:40:24 -0600

    created variables for the function

--- function.js
+++ function.js
@@ -1,2 +1,9 @@
 "use strict";
 
+//Calculate Tip
+function calulateTip() {
+    var billAmt = document.getElementById("billamt").value;
+    var serviceQual = document.getElementById("serviceQual").value;
+    var numOfPeople = document.getElementById("peopleamt").value;
+
+}
