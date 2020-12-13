5f3c7a85e4d0daa98702ee7c684110ef1a57a55e
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-12 18:12:40 -0600

    created function in javascript

--- function.js
+++ function.js
@@ -1,17 +1,16 @@
-"use strict";
+// "use strict";
 
 //Calculate Tip
-function calulateTip() {
-    var billAmt = document.getElementById("billamt").value;
-    var serviceQual = document.getElementById("serviceQual").value;
-    var numOfPeople = document.getElementById("peopleamt").value;
+function calculateTip() {
+    let billAmt = document.getElementById("billamt").value;
+    let serviceQual = document.getElementById("servicequal").value;
+    let numOfPeople = document.getElementById("peopleamt").value;
 
 //Validate input
     if (billAmt === "" || serviceQual == 0) {
         alert("Please enter values");
         return;
     }
-
 //Check see if this input is empty or less than or equal to 1
     if (numOfPeople === "" || numOfPeople <= 1) {
         numOfPeople = 1;
@@ -21,7 +20,7 @@ function calulateTip() {
     }
 
 //Calculate tip
-    var total = (billAmt * serviceQual) / numOfPeople;
+    let total = (billAmt * serviceQual) / numOfPeople;
 //Round the two decimal places
     total = Math.round(total * 100) / 100;
 //Allows us to have two digits after decimal point
@@ -29,4 +28,13 @@ function calulateTip() {
 //Display the tip
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
+
+//Hide the tip amount on load
+    document.getElementById("totalTip").style.display = "none";
+    document.getElementById("each").style.display = "none";
+
+//Click to call function
+    document.getElementById("calculate").onclick = function () {
+        calculateTip();
+    }
 }
