0322d26dc9dcca812b416b78435015c71e2d60c1
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2020-12-16 20:30:24 -0600

    completed tip calculator

--- function.js
+++ function.js
@@ -1,17 +1,15 @@
-// "use strict";
-
 //Calculate Tip
 function calculateTip() {
-    let billAmt = document.getElementById("billamt").value;
-    let serviceQual = document.getElementById("servicequal").value;
-    let numOfPeople = document.getElementById("peopleamt").value;
+    var billAmt = document.getElementById("billamt").value;
+    var serviceQual = document.getElementById("serviceQual").value;
+    var numOfPeople = document.getElementById("peopleamt").value;
 
-//Validate input
+    //validate input
     if (billAmt === "" || serviceQual == 0) {
         alert("Please enter values");
         return;
     }
-//Check see if this input is empty or less than or equal to 1
+    //Check to see if this input is empty or less than or equal to 1
     if (numOfPeople === "" || numOfPeople <= 1) {
         numOfPeople = 1;
         document.getElementById("each").style.display = "none";
@@ -19,22 +17,22 @@ function calculateTip() {
         document.getElementById("each").style.display = "block";
     }
 
-//Calculate tip
-    let total = (billAmt * serviceQual) / numOfPeople;
-//Round the two decimal places
+    //Calculate tip
+    var total = (billAmt * serviceQual) / numOfPeople;
+    //round to two decimal places
     total = Math.round(total * 100) / 100;
-//Allows us to have two digits after decimal point
+    //next line allows us to always have two digits after decimal point
     total = total.toFixed(2);
-//Display the tip
+    //Display the tip
     document.getElementById("totalTip").style.display = "block";
     document.getElementById("tip").innerHTML = total;
+}
 
 //Hide the tip amount on load
-    document.getElementById("totalTip").style.display = "none";
-    document.getElementById("each").style.display = "none";
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
 
-//Click to call function
-    document.getElementById("calculate").onclick = function () {
-        calculateTip();
-    }
-}
+//click to call function
+document.getElementById("calculate").onclick = function () {
+    calculateTip();
+};
